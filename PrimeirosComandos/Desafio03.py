7ec5120dcdf8d0025c35562b31fcb48cffd531f6
lariiferraz <129698296+lariiferraz@users.noreply.github.com>
2025-08-13 15:39:51 -0300

    Update Desafio03.py

--- PrimeirosComandos/Desafio03.py
+++ PrimeirosComandos/Desafio03.py
@@ -1,4 +1,5 @@
 # Crie um script Python que leia dois números e tente mostrar a soma entre eles.
+# Não dará certo pois é necessário da aula de Tipos Primitivos
 
 num1 = input("Digite um número: ")
 num2 = input("Digite outro número: ")
