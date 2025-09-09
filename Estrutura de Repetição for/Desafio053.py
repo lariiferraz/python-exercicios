6aa535a8db9b80dad809a7ef81dac382811b74cd
Larissa <lala.objetivo@gmail.com>
2025-09-08 21:49:13 -0300

    Estrutura de Repetição for

---
+++ Estrutura de Repetição for/Desafio053.py
@@ -0,0 +1,3 @@
+# Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo, desconsiderando os espaços.
+# Palíndromo = se ler de trás para frente, é a mesma coisa.
+# EX: APOS A SOPA , A SACADA DA CASA , A TORRE DA DERROTA , O LOBO AMA O BOLO, ANOTARAM A DATA DA MARATONA
